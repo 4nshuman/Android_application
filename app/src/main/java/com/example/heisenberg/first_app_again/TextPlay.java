46db16de14e65ee04dc2e0f219214ffac5e03b9d
heisen04 <anshuman01.tripathi@gmail.com>
2017-11-05 15:48:35 +0530

    Cleaned up the code

--- app/src/main/java/com/example/heisenberg/first_app_again/TextPlay.java
+++ app/src/main/java/com/example/heisenberg/first_app_again/TextPlay.java
@@ -18,51 +18,63 @@ import java.util.Random;
  * Created by heisenberg on 11/5/17.
  */
 
-public class TextPlay extends AppCompatActivity {
+public class TextPlay extends AppCompatActivity implements View.OnClickListener {
+
+    Button b1;
+    ToggleButton tb1;
+    EditText et;
+    TextView tv;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.text);
 
-        Button b1 = (Button) findViewById(R.id.btnResults);
-        final ToggleButton tb1 = (ToggleButton) findViewById(R.id.tBPass);
-        final EditText et = (EditText) findViewById(R.id.etxtbox);
-        final TextView tv = (TextView) findViewById(R.id.tvResults);
+        initialize_components();
 
-        tb1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(tb1.isChecked()){
-                    et.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-                }else{
-                    et.setInputType(InputType.TYPE_CLASS_TEXT);
-                }
-            }
-        });
+        tb1.setOnClickListener(this);
+        b1.setOnClickListener(this);
+    }
 
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    private void initialize_components() {
+        b1 = (Button) findViewById(R.id.btnResults);
+        tb1 = (ToggleButton) findViewById(R.id.tBPass);
+        et = (EditText) findViewById(R.id.etxtbox);
+        tv = (TextView) findViewById(R.id.tvResults);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.btnResults :
                 String str = et.getText().toString();
-                if(str.contentEquals("left")) {
+                if (str.contentEquals("left")) {
                     tv.setText("Gravity set to left");
                     tv.setGravity(Gravity.LEFT);
-                }else if(str.contentEquals("right")){
+                } else if (str.contentEquals("right")) {
                     tv.setText("Gravity set to right");
                     tv.setGravity(Gravity.RIGHT);
-                }else if(str.contentEquals("center")){
+                } else if (str.contentEquals("center")) {
                     tv.setText("Gravity set to center");
                     tv.setGravity(Gravity.CENTER);
-                }else if(str.contentEquals("blue")){
+                } else if (str.contentEquals("blue")) {
                     tv.setText("Color set to blue");
                     tv.setTextColor(Color.BLUE);
-                }else{
+                } else {
                     Random randVar = new Random();
                     tv.setText("Randomizing");
                     tv.setTextSize(randVar.nextInt(75));
-                    tv.setTextColor(Color.argb(randVar.nextInt(),randVar.nextInt(),randVar.nextInt(),randVar.nextInt()));
+                    tv.setTextColor(Color.argb(randVar.nextInt(), randVar.nextInt(), randVar.nextInt(), randVar.nextInt()));
                 }
-            }
-        });
+                break;
+
+            case R.id.tBPass :
+                if (tb1.isChecked()) {
+                    et.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                } else {
+                    et.setInputType(InputType.TYPE_CLASS_TEXT);
+                }
+                break;
+        }
     }
 }
